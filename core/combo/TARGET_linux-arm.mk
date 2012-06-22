3f425d904e2bbbd3e70a37bfe4bd3e2966716b46
Liquid Smooth <liquid0624@gmail.com>
2012-06-22 18:32:02 -0400

    Revert "Revert "Enable Strict Aliasing Build Optimization""
    This reverts commit 73ecdb44de8df8fddea5ff175df8d4b9a5cd368c.

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -79,7 +79,7 @@ ifeq ($(ARCH_ARM_HAVE_THUMB_SUPPORT),true)
 TARGET_thumb_CFLAGS :=  -mthumb \
                         -Os \
                         -fomit-frame-pointer \
-                        -fno-strict-aliasing
+                        -fstrict-aliasing
 else
 TARGET_thumb_CFLAGS := $(TARGET_arm_CFLAGS)
 endif
